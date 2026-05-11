b02ca2f9de07a5c9e9b0d8a85666f426aec8efa5
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-05-11 20:06:14 +0100

    Update BasicPlusOperatorOverloading.cpp

--- BasicPlusOperatorOverloading.cpp
+++ BasicPlusOperatorOverloading.cpp
@@ -1,4 +1,3 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 
 class Factorial
@@ -17,28 +16,47 @@ class Factorial
             
             for(int i = startnum; i>1; i--)
                 answer = answer * i;
-                
+               
+            std::cout << startnum << " factorial is : "; 
             return answer;
         }
+        Factorial operator-(Factorial s);
         Factorial operator+(Factorial s);
+        void operator++();
 };
+
+Factorial Factorial::operator-(Factorial sanj)
+{
+  Factorial answerobj(startnum - sanj.startnum);
+  return answerobj;
+}
 
 Factorial Factorial::operator+(Factorial sanj)
 {
-  startnum = startnum + sanj.startnum;
-  return *this;
+  Factorial answerobj(startnum + sanj.startnum);
+  return answerobj;
+}
+void Factorial::operator++()
+{
+  startnum = startnum + 1;
 }
 
 int main() {
     // Write C++ code here
-    Factorial obj(5);
-    Factorial obj2(2);
+    Factorial obj_six(6);
+    Factorial obj_two(2);
     
-    std::cout << obj.Calculate() << std::endl;
-    std::cout << obj2.Calculate() << std::endl;
+    std::cout << obj_six.Calculate() << std::endl;
+    std::cout << obj_two.Calculate() << std::endl;
     
-    Factorial obj3 = obj2 + obj;
-    std::cout << obj3.Calculate() << std::endl;
+    Factorial obj_eight = obj_six + obj_two;
+    std::cout << obj_eight.Calculate() << std::endl;
+    
+    Factorial obj_four = obj_six - obj_two;
+    std::cout << obj_four.Calculate() << std::endl;
+    
+    ++obj_six;
+    std::cout << obj_six.Calculate() << std::endl;
 
     return 0;
 }
